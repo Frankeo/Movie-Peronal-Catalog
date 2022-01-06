528a363ca2eafab416ccd2aa317143c6ea00b9b5
Frankeo <ingfrancisco.moreno1@gmail.com>
2022-01-06 14:39:24 +0000

    Add new function to firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,74 @@
 import * as functions from "firebase-functions";
-const cors = require('cors')({ origin: true });
+import { URLSearchParams } from "url";
+const fetch = require('node-fetch');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  cors(request, response, () => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.status(200).send("Hello from Firebase!");
-  });
+
+export interface MovieSummary {
+  id: string;
+  genrer: string;
+  poster: string;
+  title: string;
+  ranking: number;
+  sinopsis: string;
+}
+
+export interface ImDBDetailResponse {
+  id: string;
+  title: {
+    title: string;
+    image: {
+      url: string;
+    }
+  }
+  plotSummary: {
+    text: string;
+  }
+  genres: string[];
+}
+
+interface ImDBFindResponse {
+  d: ImDBResult[];
+}
+
+export interface ImDBResult {
+  id: string;
+  l: string;
+}
+
+export interface MovieTitle {
+  id: string;
+  title: string;
+}
+
+const headers = {
+  'x-rapidapi-host': 'imdb8.p.rapidapi.com',
+  'x-rapidapi-key': '9ba6592aeemsh633be5bdfd3c764p1c9d2cjsn328075020874'
+};
+
+export const getTitlesWithIds = functions.https.onCall(async (data, context) => {
+  const url = new URL("https://imdb8.p.rapidapi.com/auto-complete");
+  url.search = new URLSearchParams([['q', data]]).toString();
+
+  const response = await fetch(url.toString(), { headers });
+  const imdbResponse = await response.json() as ImDBFindResponse;
+  return imdbResponse.d.map(({ id, l }) => ({ id, title: l })) as MovieTitle[];
+});
+
+
+
+export const getMovieOverview = functions.https.onCall(async (data, context) => {
+  const url = new URL("https://imdb8.p.rapidapi.com/title/get-overview-details");
+  url.search = new URLSearchParams([['tconst', data]]).toString();
+
+  const response = await fetch(url.toString(), { headers });
+  const imdbResponse = await response.json() as ImDBDetailResponse;
+  return {
+    genrer: imdbResponse.genres[0],
+    id: imdbResponse.id,
+    poster: imdbResponse.title.image.url,
+    title: imdbResponse.title.title,
+    sinopsis: imdbResponse.plotSummary.text
+  } as MovieSummary;
 });
--- src/app/service/movie-api.service.ts
+++ src/app/service/movie-api.service.ts
@@ -1,57 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { getFunctions, httpsCallable, Functions } from '@angular/fire/functions';
-import { map, Observable, of } from 'rxjs';
+import { httpsCallable, Functions } from '@angular/fire/functions';
+import { Observable, from } from 'rxjs';
 
-import { ImDBResult, ImDBFindResponse, MovieSummary, ImDBDetailResponse, MovieTitle } from '../interfaces/interfaces';
+import { MovieSummary, MovieTitle } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieApiService {
-  private url = "https://imdb8.p.rapidapi.com";
-  private headers = {
-    'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-    'x-rapidapi-key': '9ba6592aeemsh633be5bdfd3c764p1c9d2cjsn328075020874'
-  };
-  constructor(private httpClient: HttpClient, private func: Functions) { }
+  constructor(private func: Functions) { }
 
   getTitlesWithIds(partialName: string): Observable<MovieTitle[]> {
-    this.httpClient.get("http://127.0.0.1:5001/movie-personal-catalog/us-central1/helloWorld", {
-      headers: {
-        'Access-Control-Allow-Origin' : '*'
-      },
-      responseType: 'text'
-    }).subscribe(resp => {
-      console.log('data', resp.toString());      
-    });
-    // const titles = httpsCallable(this.func, 'helloWorld');
-    // titles().then((result) => { 
-    //   const data = result.data;
-    //   console.log('data', data);
-    // });
-    return of<MovieTitle[]>([{ id: '1', title: 'bla'}]);
-    /*return this.httpClient.get<ImDBFindResponse>(`${this.url}/auto-complete`, {
-      headers: this.headers, params: { q: partialName }})
-      .pipe(
-        map(list => list.d.map(({ id, l }) => ({ id, title: l })))
-    );*/
+    const getTitles = httpsCallable(this.func, 'getTitlesWithIds');
+    return from(getTitles(partialName).then(result => result.data as MovieTitle[]));
   }
 
   getMovieOverview(id: string): Observable<MovieSummary> {
-     return this.httpClient.get<ImDBDetailResponse>(`${this.url}/title/get-overview-details`, {
-      headers: this.headers, params: { tconst: id }})
-      .pipe(
-        map(result => {
-          console.log(result);
-          return ({
-          genrer: result.genres[0],
-          id: result.id,
-          poster: result.title.image.url,
-          title: result.title.title,
-          sinopsis: result.plotSummary.text
-        }) as MovieSummary; }
-        )
-      );
+    const getTitles = httpsCallable(this.func, 'getMovieOverview');
+    return from(getTitles(id).then(result => result.data as MovieSummary));
   }
 }
